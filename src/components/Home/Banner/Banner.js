cef3d4a3f96dacc8d4f6bdfffd606654c7d00c94
Rayhan-Ahmed-7 <dev.rayhan7@gmail.com>
2022-05-05 16:02:58 +0600

    banner section added

--- src/components/Home/Banner/Banner.js
+++ src/components/Home/Banner/Banner.js
@@ -8,7 +8,7 @@ const Banner = () => {
                 <small className='text-lg l-spacing text-[#2B2B2B] font-medium'>TECH SCOPE</small>
                 <h2 className='md:text-6xl text-4xl font-fast py-4 text-[#2B2B2B]'>ELECTRONICS IS <br /> <span className='text-lightgreen'>THE FUTURE</span></h2>
                 <p className='text-[#2B2B2B] text-lg tracking-tight text-justify'>Electronic devices helps us to enhance speaking, listening, and creative skills. Ease of Access to Information. Saves Time. Ease of Mobility. Cost Efficiency.These make our lives more convenient and easy and builds communication faster and easier.</p>
-                <button className='my-4 px-4 py-2 bg-[#2BB673] rounded-sm text-white'>CHECKOUT NOW</button>
+                <button className='my-4 px-4 py-2 bg-[#2BB673] rounded-sm text-white hover:bg-[#24d881]'>CHECKOUT NOW</button>
             </div>
             <div className='flex-1 md:order-2 order-1'>
                 <img src={banner} alt="" />
