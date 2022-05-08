b387d9d1d9bccdf2bdd0d6b0aad9f05afe8db67e
Rayhan-Ahmed-7 <dev.rayhan7@gmail.com>
2022-05-08 17:27:46 +0600

    header responsive issue solved

--- src/components/Shared/Header/Header.js
+++ src/components/Shared/Header/Header.js
@@ -14,7 +14,7 @@ const Header = () => {
         signOut(auth)
     }
     return (
-        <header className='py-4 px-2 bg-white custom-shadow'>
+        <header className='py-4 bg-white custom-shadow'>
             <nav className='w-11/12 mx-auto flex justify-between items-center relative'>
                 <div className="logo">
                     <Link to='/'>
@@ -22,14 +22,14 @@ const Header = () => {
                     </Link>
                 </div>
                 {/* <div className=''> */}
-                    <span onClick={() => setVisible(!visible)} className='md:hidden visible cursor-pointer'>
+                    <span onClick={() => setVisible(!visible)} className='lg:hidden visible cursor-pointer'>
                         {!visible ?
                             <RiMenu4Fill className='text-3xl'></RiMenu4Fill>
                             :
                             <RiCloseFill className='text-3xl'></RiCloseFill>
                         }
                     </span>
-                    <ul className={`${visible ? 'opacity-100 right-2' : 'opacity-100 -right-64'} flex flex-col md:flex-row md:space-x-6 space-x-0 md:space-y-0 space-y-4 text-lg absolute md:static top-10 right-2 md:bg-transparent md:opacity-100 bg-white/80 backdrop-blur-sm rounded-md p-5 md:p-0 transition-all duration-300 z-100 md:items-center md:w-auto w-52 border-y-4 md:border-0 border-y-rose-200`}>
+                    <ul className={`${visible ? 'opacity-100 right-2' : 'opacity-100 -right-64'} flex flex-col lg:flex-row lg:space-x-6 space-x-0 lg:space-y-0 space-y-4 text-lg absolute lg:static top-10 right-2 lg:bg-transparent lg:opacity-100 bg-white/80 backdrop-blur-sm rounded-md p-5 lg:p-0 transition-all duration-300 z-100 lg:items-center lg:w-auto w-52 border-y-4 lg:border-0 border-y-rose-200`}>
                         <li>
                             <NavLink
                                 to='/'
